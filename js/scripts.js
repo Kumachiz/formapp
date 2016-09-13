f4600db0b8ac464226a70d57a1f0fdfed215c319
Andrew Erickson and Michae Munyaneza <michae@>
2016-09-12 20:28:20 -0700

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var beverage = $("#businessType").val();
+
+var flavor = $("input:radio[name=subscription]:checked").val();subscription
+
+var dob = $("#startdate").val();
+
+var favoriteColor = $("#color").val();
